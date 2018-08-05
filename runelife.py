1c918656df112dfec586fa3839c4887a4478d769
Stormy922 <38141912+Stormy922@users.noreply.github.com>
2018-08-05 03:10:59 +0200

    runelife.py

---
+++ runelife.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
+
+
+Client = discord.Client() #Initialise Client 
+client = commands.Bot(command_prefix = "!") #Initialise client bot
+
+@client.event 
+async def on_ready():
+    print("Der Bot ist online und bereit sich zu verbinden mit Discord") 
+
+@client.event
+async def on_message(message):
+    if message.content.upper().startswith('!PING'):
+        userID = message.author.id
+        await client.send_message(message.channel, "<@%s> Pong!" % (userID))
+
+client.run("NDc1MzU4OTQ1OTA1NDc1NjA1.DkfBJQ.SEBQqoaeISrBuklQv6Xx2XL3wVM")
