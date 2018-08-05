f854e320be0715469d017df8058890cc383a1170
Stormy922 <38141912+Stormy922@users.noreply.github.com>
2018-08-05 04:41:14 +0200

    Update runelife.py

--- runelife.py
+++ runelife.py
@@ -6,13 +6,19 @@ import time
 
 
 Client = discord.Client() #Initialise Client 
-client = commands.Bot(command_prefix = "!") #Initialise client bot
+client = commands.Bot(command_prefix = ".") #Initialise client bot
 
 @client.event 
 async def on_ready():
     print("Der Bot ist online und bereit sich zu verbinden mit Discord")
 
-@client.command(pass_context=True)
+@client.event
+async def on_message(message):
+    if message.content.upper().startswith('.PING'):
+        userID = message.author.id
+        await client.send_message(message.channel, "<@%s> Pong!" % (userID))
+        
+        @client.command(pass_context=True)
 async def clear(ctx, clear=100)
     channel = ctx.message.channel
     message = []
@@ -21,11 +27,5 @@ async def clear(ctx, clear=100)
     await client.delete_messages(messages)
     await client.say('Nachricht Gelöscht.')
     await client.process_commands(messages
-
-@client.event
-async def on_message(message):
-    if message.content.upper().startswith('!PING'):
-        userID = message.author.id
-        await client.send_message(message.channel, "<@%s> Pong!" % (userID))
 
 client.run("NDc1MzU4OTQ1OTA1NDc1NjA1.DkfBJQ.SEBQqoaeISrBuklQv6Xx2XL3wVM")
