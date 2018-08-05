1550bc30604aa8ed87d9178a7707bea07acc4cfa
Stormy922 <38141912+Stormy922@users.noreply.github.com>
2018-08-05 05:18:25 +0200

    Update runelife.py

--- runelife.py
+++ runelife.py
@@ -1,26 +1,20 @@
 import discord
-from discord.ext.commands import Bot
-from discord.ext import commands
 import asyncio
-import time
 
-TOKEN = 'NDc1MzU4OTQ1OTA1NDc1NjA1.DkfBJQ.SEBQqoaeISrBuklQv6Xx2XL3wVM'
+client = discord.Client()
 
-@client.event 
+@client.event
 async def on_ready():
-    print("Der Bot ist online und bereit sich zu verbinden mit Discord")
-    
-   Client = Bot('!')
+    print('Eingeloggt als')
+    print(client.user.name)
+    print(client.user.id)
+    print('-----------')
 
-@Client.command(pass_context = True)
-async def clear(ctx, number):
-    number = int(number) #Converting the amount of messages to delete to an integer
-    counter = 0
-    async for x in Client.logs_from(ctx.message.channel, limit = number):
-        if counter < number:
-            await Client.delete_message(x)
-            counter += 1
-            await asyncio.sleep(1.2) #1.2 second timer so the deleting process can be even
- 
-    
-client.run("TOKEN")
+
+@client.event
+async def on_message(message):
+    if message.content.lower().startswith('?test'):
+        await client.send_message(message.channel, "Test bestanden")
+
+
+client.run('NDc1MzU4OTQ1OTA1NDc1NjA1.DkfBJQ.SEBQqoaeISrBuklQv6Xx2XL3wVM')
